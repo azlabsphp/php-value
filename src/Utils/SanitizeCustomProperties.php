fb5684ad6a8ada5ff7daccdad43269ac194102bb
azandrew-sidoine <asmyns.platonnas29@gmail.com>
2023-06-01 11:37:58 +0000

    Added a SanitizeCustomProperties utility class to the library

---
+++ src/Utils/SanitizeCustomProperties.php
@@ -0,0 +1,104 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the drewlabs namespace.
+ *
+ * (c) Sidoine Azandrew <azandrewdevelopper@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Drewlabs\PHPValue\Utils;
+
+class SanitizeCustomProperties
+{
+    /**
+     * @var bool
+     */
+    private $sort = false;
+
+    /**
+     * Creates class instance
+     * 
+     * @param bool $sort 
+     */
+    public function __construct(bool $sort = false)
+    {
+        $this->sort = $sort;
+    }
+
+    /**
+     * Creates list of custom properties names removing `invalid` names
+     * from the provided list of values
+     * 
+     * @param array|iterable|\Traversable $values 
+     * @return array 
+     */
+    public function __invoke($values): array
+    {
+        $array = iterator_to_array($this->createIterator($values));
+        if ($this->sort) {
+            sort($array);
+        }
+        return $array;
+    }
+
+    /**
+     * Creates list of custom properties names removing `invalid` names
+     * from the provided list of values
+     * 
+     * @param array|iterable|\Traversable $values 
+     * @return array 
+     */
+    public function call($values): array
+    {
+        $array = iterator_to_array($this->createIterator($values));
+        if ($this->sort) {
+            sort($array);
+        }
+        return $array;
+    }
+
+    /**
+     * Creates an iterable of properties
+     * 
+     * @param array|iterable|\Traversable $properties 
+     * @return \Traversable<int, mixed, mixed, void> 
+     */
+    private function createIterator($properties)
+    {
+        $array = [];
+
+        foreach ($properties as $property) {
+            // escape the property if it equals * or it's a float, decimal or integer value
+            if (($property === '*') || is_numeric($property) ) {
+                continue;
+            }
+
+            // property names must be a valid `PHP` string
+            if (!is_string($property)) {
+                continue;
+            }
+
+            // we do not want to inclue duplicated properties, so we exclude the property if it was already handled
+            if (false !== array_search($property, $array)) {
+                continue;
+            }
+
+            $array[] = $property;
+
+            // Case the property contains `.` character, return the string before it
+            if (false !== strpos($property, '.')) {
+                yield explode('.', $property)[0];
+                continue;
+            }
+
+            // By default yiel the property value as it is
+            yield $property;
+        }
+
+    }
+}
---
+++ tests/SanitizeCustomPropertiesTest.php
@@ -0,0 +1,26 @@
+<?php
+
+use Drewlabs\PHPValue\Utils\SanitizeCustomProperties;
+use PHPUnit\Framework\TestCase;
+
+class SanitizeCustomPropertiesTest extends TestCase
+{
+    public function test_sanitize_custom_properties__invoke()
+    {
+        $result = (new SanitizeCustomProperties(sort: true))(['product.type', 'name', 12, 'ratings']);
+        $this->assertEquals(['name', 'product', 'ratings'], $result);
+    }
+
+    public function test_sanitize_custom_properties_call()
+    {
+        $result = (new SanitizeCustomProperties(sort: true))->call(['product.type', 'name', 12, 'ratings']);
+        $this->assertEquals(['name', 'product', 'ratings'], $result);
+    }
+
+    public function test_sanitize_custom_properties_revomes_duplicates()
+    {
+        $result = (new SanitizeCustomProperties(sort: true))->call(['product.type', 'name', 'product.type', 'ratings', 'name']);
+        $this->assertEquals(['name', 'product', 'ratings'], $result);
+
+    }
+}
